62df38b424c1e4f6b749fca198c128c31aa14bef
owais897 <m.owais2118@gmail.com>
2021-06-18 21:38:36 +0500

    refactor code and add vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,92 +1,36 @@
 <template>
 <div class="container">
   <Header @toggle-add-task="toggleAddTask" :showAddTask="showAddTask" title="Task Tracker"></Header>
-  <div v-show="showAddTask">
-    <AddTask @add-task="addTask"></AddTask>
-  </div>
-  <Tasks  @toggle-reminder="toggleReminder"  @delete-task="deleteTask" :tasks="tasks"/>
+
+  <router-view :showAddTask="showAddTask">
+
+  </router-view>
+  <Footer/>
 </div>
 </template>
 
 <script>
 
 import Header from "./components/Header.vue"
-import Tasks from "./components/Tasks.vue"
-import AddTask from "./components/AddTask.vue"
+import Footer from "./components/Footer.vue"
+
 export default {
   name: 'App',
   components: {
   Header,
-  Tasks,
-
-  AddTask
+Footer,
   },
   data(){
     return {
-      tasks :[],
       showAddTask:false 
     }
   },
   methods:{
-    toggleAddTask(){
+     toggleAddTask(){
       this.showAddTask=!this.showAddTask 
     },
-    async addTask(task){
-      const res =await fetch('api/tasks',{
-        method:"POST",
-        headers:{
-          "Content-type":"application/json"
-        },
-        body:JSON.stringify(task)
-      })
-      const data = await res.json()
-      console.log('data in add task: ', data);
-      this.tasks=[...this.tasks,data]
-    },
-    async deleteTask(id){
-      if(confirm("are you sure?")){
-           const res =await fetch(`api/tasks/${id}`,{
-        method:"DELETE",
-       
-      })
-
-        res.status===200?
-      (this.tasks=this.tasks.filter(v=>v.id!=id))
-      :alert("error in deleting")}
-      console.log('id llllllllllll: ', id);
-
-    },
-    async toggleReminder(id){
-      const taskToToggle= await this.fetchTask(id)
-      const updTask = {...taskToToggle,reminder:!taskToToggle.reminder}
-      const res =await fetch(`api/tasks/${id}`,{
-        method:"PUT",
-        headers:{
-          "Content-type":"application/json"
-
-        },
-        body:JSON.stringify(updTask)
-
-      })
-      const data = await res.json()
-      console.log('data toggle: ', data);
-      this.tasks = this.tasks.map(task=>task.id===id?{...task,reminder:data.reminder}:task)
-      console.log('this.tasks: ', this.tasks);
-    },
-    async fetchTasks(){
-      const res = await fetch("api/tasks")
-      const data = await res.json()
-      return data 
-    },
-    async fetchTask(id){
-      const res = await fetch(`api/tasks/${id}`)
-      const data = await res.json()
-      return data 
-    }
   },
- async created(){
-    this.tasks = await this.fetchTasks()
-  }
+
 }
 </script>
 
---
+++ src/views/About.vue
@@ -0,0 +1,8 @@
+<template>
+    <h3>
+        version 1.0.0
+    </h3>
+    <router-link to="/">
+        go back
+    </router-link>
+</template>
